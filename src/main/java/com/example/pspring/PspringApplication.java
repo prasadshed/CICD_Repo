e5e6619b9fa076e9fa0954e22f9e6c5c6a253939
Prasad Shedage <prasadshedage1998@gmail.com>
2026-02-08 14:46:31 +0530

    adding pathvariable approach

--- src/main/java/com/example/pspring/PspringApplication.java
+++ src/main/java/com/example/pspring/PspringApplication.java
@@ -3,15 +3,16 @@ package com.example.pspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class PspringApplication {
 	
-	@GetMapping("/welcome")
-	public String welcome() {
-		return "Hello welcome";
+	@GetMapping("/welcome/{id}")
+	public String welcome(@PathVariable int id) {
+		return "Hello welcome"+ id;
 	}
 
 	public static void main(String[] args) {
