f2dd7f77bef16089c542d6bac3c11f39788e78cb
Prasad Shedage <prasadshedage1998@gmail.com>
2026-02-08 13:42:00 +0530

    Intial commit

---
+++ src/main/java/com/example/pspring/PspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.pspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pspring/PspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
