661403440fa53419450225d6504faa1f0ac9dffd
Prasad Shedage <prasadshedage1998@gmail.com>
2026-03-01 15:11:55 +0530

    2 methods comment out

--- src/main/java/com/example/pspring/PspringApplication.java
+++ src/main/java/com/example/pspring/PspringApplication.java
@@ -10,18 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PspringApplication {
 	
-	@GetMapping("/welcome/{id}")
-	public String welcome(@PathVariable int id) {
-		return "Hello welcome"+ id;
-	}
+//	@GetMapping("/welcome/{id}")
+//	public String welcome(@PathVariable int id) {
+//		return "Hello welcome"+ id;
+//	}
 	@GetMapping("/welcome/{name}")
 	public String welc(@PathVariable String name) {
 		return "Hello welcome"+ name;
 	}
-	@GetMapping("/welcome/{dou}")
-	public String welc2(@PathVariable Double dou) {
-		return "Hello welcome"+ dou;   /////returning hello welcome statement
-	}
+//	@GetMapping("/welcome/{dou}")
+//	public String welc2(@PathVariable Double dou) {
+//		return "Hello welcome"+ dou;   /////returning hello welcome statement
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(PspringApplication.class, args);
 	}
