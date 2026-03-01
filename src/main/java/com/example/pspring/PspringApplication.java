fe21ca57fadd3fb1de012cd3c751e4fcbf23a871
Prasad Shedage <prasadshedage1998@gmail.com>
2026-03-01 15:01:41 +0530

    few change

--- src/main/java/com/example/pspring/PspringApplication.java
+++ src/main/java/com/example/pspring/PspringApplication.java
@@ -14,12 +14,12 @@ public class PspringApplication {
 	public String welcome(@PathVariable int id) {
 		return "Hello welcome"+ id;
 	}
-	@GetMapping("/welcome{name}")
-	public String welcome1(@PathVariable String name) {
+	@GetMapping("/welcome/{name}")
+	public String welc(@PathVariable String name) {
 		return "Hello welcome"+ name;
 	}
-	@GetMapping("/welcome{dou}")
-	public String welcome2(@PathVariable Double dou) {
+	@GetMapping("/welcome/{dou}")
+	public String welc2(@PathVariable Double dou) {
 		return "Hello welcome"+ dou;   /////returning hello welcome statement
 	}
 	public static void main(String[] args) {
